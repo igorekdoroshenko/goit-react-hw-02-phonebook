83b8d0595697bef8e986a1dfd24fe31a4e7477ad
igorekdoroshenko <igorek.doroshenko@gmail.com>
2023-05-29 23:05:25 +0300

    Finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,79 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Container, Title, SubTitle } from './App.style'
 import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
+import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
+
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
+  // Додавання нового контакту в список контактів
+  formSubmitHandler = data => {
+    const { name } = data;
+    const { contacts } = this.state;
 
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
+    const isContactExists = contacts.some(contact => contact.name === name);
+
+    if (isContactExists) {
+      alert(`${name} is already in contacts`);
+    } else {
+      // Додавання нового контакта
+      const newContact = { id: nanoid(), ...data };
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+  // Зміна значення фільтра
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  // Отримання відфільтрованих контактів
+  getFilterContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  // Видалення контакту зі списку
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  render() {
+    const filterContacts = this.getFilterContacts();
+    const { filter } = this.state;
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <SubTitle>Contacts</SubTitle>
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+        <ContactList
+          contacts={filterContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
     );
   }
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import { Form, Label, Button, Input } from './ContactForm.style'
 
 export class ContactForm extends Component {
   state = {
@@ -6,32 +9,58 @@ export class ContactForm extends Component {
     number: '',
   };
 
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+      this.props.onSubmit({ name: this.state.name, number: this.state.number });
+      this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  nameId = nanoid();
+  numberId = nanoid();
+
   render() {
     return (
-      <form>
-        <label htmlFor="">
+      <Form onSubmit={this.handleSubmit}>
+        <Label htmlFor={this.nameId}>
           Name
-          <input
+          <Input
             type="text"
             name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
-        </label>
-        <label htmlFor="">
+        </Label>
+        <Label htmlFor={this.numberId}>
           Number
-          <input
+          <Input
             type="tel"
             name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
-            </label>
-            
-           <button type="submit">Add contact</button> 
-      </form>
+        </Label>
+
+        <Button type="submit">Add contact</Button>
+      </Form>
     );
   }
 }
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
